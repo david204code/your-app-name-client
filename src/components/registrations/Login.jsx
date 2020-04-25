1212fd330485b18e49b8f4670763d80b433b4fe6
David Wu <david204code@gmail.com>
2020-04-25 23:28:11 +0100

    add axios post to the Login component

--- src/components/registrations/Login.jsx
+++ src/components/registrations/Login.jsx
@@ -23,7 +23,43 @@ class Login extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault()
-  };
+    const { username, email, password } = this.state;
+
+    let user = {
+      username: username,
+      email: email,
+      password: password
+    }
+
+    axios.post('http://localhost:3001/login', {user}, {withCredentials: true})
+      .then(response => {
+        if (response.data.logged_in) {
+          this.props.handleLogin(response.data)
+          this.redirect()
+        } else {
+          this.setState ({
+            errors: response.data.errors
+          })
+        }
+      })
+      .catch(error => console.log('api errors:', error))
+    };
+
+    redirect = () => {
+      this.props.history.push('/')
+    }
+
+    handleErrors = () => {
+      return (
+        <div>
+          <ul>
+            {this.state.errors.map(error => {
+              return <li key={error}>{error}</li>
+            })}
+          </ul>
+        </div>
+      )
+    }  
 
   render() {
     const { username, email, password } = this.state;
@@ -64,10 +100,14 @@ class Login extends React.Component {
           </div> 
         </form>
 
+        <div>
+          {
+            this.state.errors ? this.handleErrors() :null
+          }
+        </div>
       </div>
     );
   };
-
 };
 
 export default Login;
